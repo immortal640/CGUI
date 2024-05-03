909f34c95f51aedd07aefae71ecd1180f258cf9b
immortal640 <168854403+immortal640@users.noreply.github.com>
2024-05-03 19:21:39 +0200

    Add files via upload

---
+++ CGUI/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGUI
+{
+    public class Cursor
+    {
+        public static Point currPosNDiv;
+        public static Point lastPosNDiv;
+        public static Point currPosDiv;
+        public static Point lastPosDiv;
+        public static Point maxCurPos;
+        public static Point minCurPos;
+
+        public static int Initialize()
+        {
+            ;
+            return 0;
+        }
+    }
+}
---
+++ CGUI/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGUI
+{
+    public class Message
+    {
+        public static int Custom(string msg, bool endl, ConsoleColor colorFG, ConsoleColor colorBG = ConsoleColor.Black)
+        {
+            ConsoleColor colorFGold = Console.ForegroundColor;
+            ConsoleColor colorBGold = Console.BackgroundColor;
+            Console.ForegroundColor = colorFG;
+            Console.BackgroundColor = colorBG;
+            return 0;
+        }
+    }
+}
